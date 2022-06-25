13d05e4819db266c14a01e8c33186ee1a705030f
quasemago <brunoronningfn@gmail.com>
2022-06-24 20:31:29 -0400

    feat(maps-files-manager): Implement functions for map files manager

--- src/main/java/br/com/zrage/serverdownloader/core/MapManager.java
+++ src/main/java/br/com/zrage/serverdownloader/core/MapManager.java
@@ -4,12 +4,14 @@ import br.com.zrage.serverdownloader.core.models.GameMap;
 import br.com.zrage.serverdownloader.core.models.GameServer;
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 import org.apache.commons.compress.utils.IOUtils;
+import org.apache.commons.io.FileUtils;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
 import java.io.*;
+import java.lang.annotation.ElementType;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -18,11 +20,11 @@ import java.util.UUID;
 
 public class MapManager {
     private static final Path mainTempFolder = Paths.get(System.getProperty("java.io.tmpdir")).resolve("zrageTempMaps");
-    private Path tempFolder;
-    private String mapsDirectory;
-    private GameServer serverContext;
+    private final GameServer serverContext;
+    private final Path tempFolder;
     private boolean canceled;
 
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     public MapManager(GameServer server) {
         this.tempFolder = mainTempFolder.resolve(UUID.randomUUID().toString().toUpperCase());
         new File(tempFolder.toString()).mkdirs();
@@ -32,32 +34,55 @@ public class MapManager {
     }
 
     public void download(GameMap map) {
-        // TODO:
+        String tempFile = tempFolder.resolve(map.getDownloadableFileName()).toString();
+        WebClient client = WebClient.builder()
+                .baseUrl(map.getRemoteFileName())
+                .build();
+        Flux<DataBuffer> dataBufferFlux = client.get().retrieve().bodyToFlux(DataBuffer.class);
+        DataBufferUtils.write(dataBufferFlux, Paths.get(tempFile), StandardOpenOption.CREATE).block();
     }
 
-    public void decompress(GameMap map) {
-        // TODO:
+    public void decompress(GameMap map) throws IOException {
+        String tempFile = tempFolder.resolve(map.getDownloadableFileName()).toString();
+        String decompressTempFile = tempFolder.resolve(map.getLocalFileName()).toString();
+
+        var input = new BZip2CompressorInputStream(new BufferedInputStream(new FileInputStream(tempFile)));
+        var output = new FileOutputStream(decompressTempFile);
+        try (input; output) {
+            IOUtils.copy(input, output);
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
     }
 
     public void moveToMapsFolder(GameMap map) {
-        // TODO:
+        var mapsFolder = Paths.get(utils.getGameAppMapsDirectory(serverContext));
+        if (!Files.exists(mapsFolder)) {
+            return;
+        }
+
+        var tempFile = tempFolder.resolve(map.getLocalFileName());
+        var finalFile = mapsFolder.resolve(map.getLocalFileName());
+
+        try {
+            Files.deleteIfExists(finalFile);
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
+
+        try {
+            Files.move(tempFile, finalFile);
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
     }
 
     public static void deleteAllTempFiles() {
-        // TODO:
-    }
-
-    private static void tryDeleteFile(String file) {
-        // TODO:
-    }
-
-    public static String getLastMapsFolder() {
-        // TODO:
-        return "";
-    }
-
-    public static void saveLastMapsFolder(String mapsFolder) {
-        // TODO:
+        try {
+            FileUtils.deleteDirectory(mainTempFolder.toFile());
+        } catch (IOException err) {
+            err.printStackTrace();
+        }
     }
 
     public boolean isCanceled() {
--- src/test/java/br/com/zrage/serverdownloader/ZRageServerDownloaderApplicationTests.java
+++ src/test/java/br/com/zrage/serverdownloader/ZRageServerDownloaderApplicationTests.java
@@ -1,15 +1,37 @@
 package br.com.zrage.serverdownloader;
 
+import br.com.zrage.serverdownloader.core.MapManager;
+import br.com.zrage.serverdownloader.core.models.GameMap;
+import br.com.zrage.serverdownloader.core.models.GameServer;
+import br.com.zrage.serverdownloader.core.models.ServersJson;
+import br.com.zrage.serverdownloader.core.utils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
 import java.io.IOException;
 
 @SpringBootTest
 class ZRageServerDownloaderApplicationTests {
 
-	@Test
-	void contextLoads() {
+    @Test
+    void contextLoads() throws IOException {
+        // Get ze server from json.
+        ServersJson serversJson = utils.getServersJson();
+        GameServer zeServer = serversJson.getServerList().get(0);
 
-	}
+        // Test download map.
+        GameMap map = new GameMap("ze_Pirates_Port_Royal_v5_6", zeServer);
+        MapManager mapManager = new MapManager(zeServer);
+        mapManager.download(map);
+
+        // Test decompress map.
+        mapManager.decompress(map);
+
+        // Move to maps directory.
+        mapManager.moveToMapsFolder(map);
+
+        // Delete temp folder after download.
+        MapManager.deleteAllTempFiles();
+    }
 }
